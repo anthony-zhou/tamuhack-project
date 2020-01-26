7bc46fefe7ecf14e9233af7269300a753aa89c8a
Daniel Ting <professorredstone917@gmail.com>
2020-01-25 19:56:51 -0600

    Average emotion values

--- hackathon_starter/hackathon/scripts/images.py
+++ hackathon_starter/hackathon/scripts/images.py
@@ -11,9 +11,12 @@ RECOG_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
 
 def process_image_search(query):
     """Search for images and return their emotional attributes."""
+    # Get image search results
     client = ImageSearchClient(endpoint=SEARCH_URL, credentials=CognitiveServicesCredentials(SEARCH_KEY))
     image_results = client.images.search(query=query, count=SEARCH_COUNT)
 
+    # Process image search results
+    detected = 0
     attributes = {'anger': 0,
                   'contempt': 0,
                   'disgust': 0,
@@ -30,7 +33,12 @@ def process_image_search(query):
         headers = {'Ocp-Apim-Subscription-Key': RECOG_KEY, 'Content-Type': 'application/json'}
         faces = json.loads(requests.post(RECOG_URL, params=params, json=data, headers=headers).text)
         for face in faces:
+            detected += 1
             for attribute in face['faceAttributes']['emotion']:
                 attributes[attribute] += face['faceAttributes']['emotion'][attribute]
+
+    # Average attributes
+    for attribute in attributes:
+        attributes[attribute] /= detected
 
     return attributes
