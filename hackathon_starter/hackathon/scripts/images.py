481df5bcb0674aef8b7fde55f0b30aa5b51a9a90
Daniel Ting <professorredstone917@gmail.com>
2020-01-25 19:49:28 -0600

    Add image processing

---
+++ hackathon_starter/hackathon/scripts/images.py
@@ -0,0 +1,36 @@
+from azure.cognitiveservices.search.imagesearch import ImageSearchClient
+from msrest.authentication import CognitiveServicesCredentials
+import requests
+import json
+
+SEARCH_KEY = 'e7fd0fca21264303b7d7757f3fb0d40b'
+SEARCH_URL = 'https://api.cognitive.microsoft.com'
+SEARCH_COUNT = 3
+RECOG_KEY = '6482c5eb68a0458790882a6485213569'
+RECOG_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+
+def process_image_search(query):
+    """Search for images and return their emotional attributes."""
+    client = ImageSearchClient(endpoint=SEARCH_URL, credentials=CognitiveServicesCredentials(SEARCH_KEY))
+    image_results = client.images.search(query=query, count=SEARCH_COUNT)
+
+    attributes = {'anger': 0,
+                  'contempt': 0,
+                  'disgust': 0,
+                  'fear': 0,
+                  'happiness': 0,
+                  'fear': 0,
+                  'neutral': 0,
+                  'sadness': 0,
+                  'surprise': 0,
+                 }
+    for result in image_results.value:
+        params = {'returnFaceAttributes': 'emotion', 'recognitionModel': 'recognition_02'}
+        data = {'url': result.content_url}
+        headers = {'Ocp-Apim-Subscription-Key': RECOG_KEY, 'Content-Type': 'application/json'}
+        faces = json.loads(requests.post(RECOG_URL, params=params, json=data, headers=headers).text)
+        for face in faces:
+            for attribute in face['faceAttributes']['emotion']:
+                attributes[attribute] += face['faceAttributes']['emotion'][attribute]
+
+    return attributes
